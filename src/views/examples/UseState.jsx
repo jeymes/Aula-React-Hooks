885b4af7dbe19402dc495bad11c2b840f58109b7
Jeymes Sousa <jsvpn2017@gmail.com>
2022-06-03 13:38:31 -0300

    Exercicio01 contador

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SelectionTitle from '../../components/layout/SectionTitle';
 
 const UseState = (props) => {
+
+    const [count, setCount] = useState(0)
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
+                subtitle="Estado em componentes funcionais!" />
+
+            <SelectionTitle title="Exercicio #01" />
+
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button onClick={() => setCount (count -1)} className="btn">-1</button>
+                    <button onClick={() => setCount (count +1)} className="btn">+1</button>
+                    <button onClick={() => setCount (currect => currect +1000 )} className="btn">+1000</button>
+                </div>
+            </div>
+
+
+            <SelectionTitle title="Exercicio #02" />
         </div>
     )
 }
